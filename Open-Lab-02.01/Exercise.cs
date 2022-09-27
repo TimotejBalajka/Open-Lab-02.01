7b3ec7854c4fbca9c1c4c36fa03a8db70c7386b4
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-09-27 09:13:55 +0200

    Update Exercise.cs

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -8,13 +8,11 @@ namespace Open_Lab_02._01
         {
             double[] numbers = {1, 2, 3, 4, 5};
             return numbers;
-            throw new NotImplementedException();
         }
 
         public double GetSecond(double[] numbers)
         {
             return numbers[1];
-            throw new NotImplementedException();
         }
     }
 }
